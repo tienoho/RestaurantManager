7c2230102207fbd84efc0f97310fb3ecaf9ce61a
duocnc <coduoc2502@gmail.com>
2020-12-02 20:12:43 +0700

    duocnc

--- RestaurantManager/Views/Print/frmPrint.cs
+++ RestaurantManager/Views/Print/frmPrint.cs
@@ -61,6 +61,16 @@ namespace RestaurantManager.Views.Print
         public void PrintGIAOHANG(GIAOHANG_ViewModel obj, List<D_GIAOHANG_ViewModel> data)
         {
             ReportGIAOHANG report = new ReportGIAOHANG();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="data"></param>
+        public void PrintPNHAP(PNHAP_ViewModel obj, List<D_PNHAP_ViewModel> data)
+        {
+            ReportPNHAP report = new ReportPNHAP();
             //Hide paramater
             foreach (DevExpress.XtraReports.Parameters.Parameter p in report.Parameters)
                 p.Visible = false;
