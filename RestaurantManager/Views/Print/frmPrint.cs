64380af58b46fa40af67e92fe73a31ba0a246917
sunhousedev02 <sunhousedev02@hotmail.com>
2020-12-02 22:53:52 +0700

    tienlv1

--- RestaurantManager/Views/Print/frmPrint.cs
+++ RestaurantManager/Views/Print/frmPrint.cs
@@ -31,6 +31,17 @@ namespace RestaurantManager.Views.Print
             documentViewer1.DocumentSource = report;
             report.CreateDocument();
         }
+        public void PrintDONDATDA(DONDATDA_ViewModel obj, List<D_DONDATDA_ViewModel> data)
+        {
+            ReportDONDATDA report = new ReportDONDATDA();
+            //Hide paramater
+            foreach (DevExpress.XtraReports.Parameters.Parameter p in report.Parameters)
+                p.Visible = false;
+            //Init data report
+            report.InitData(obj, data);
+            documentViewer1.DocumentSource = report;
+            report.CreateDocument();
+        }
         /// <summary>
         /// 
         /// </summary>
@@ -64,10 +75,10 @@ namespace RestaurantManager.Views.Print
         /// </summary>
         /// <param name="obj"></param>
         /// <param name="data"></param>
-        public void PrintPNHAP(PNHAP_ViewModel obj, List<D_PNHAP_ViewModel> data)
-        {
-            ReportPNHAP report = new ReportPNHAP();
-        }
+        //public void PrintPNHAP(PNHAP_ViewModel obj, List<D_PNHAP_ViewModel> data)
+        //{
+        //    ReportPNHAP report = new ReportPNHAP();
+        //}
         public void PrintGIAOHANG(GIAOHANG_ViewModel obj, List<D_GIAOHANG_ViewModel> data)
         {
             ReportGIAOHANG report = new ReportGIAOHANG();
@@ -100,12 +111,11 @@ namespace RestaurantManager.Views.Print
         public void PrintPNHAP(PNHAP_ViewModel obj, List<D_PNHAP_ViewModel> data)
         {
             ReportPNHAP report = new ReportPNHAP();
-            ReportBCDoanhThu report = new ReportBCDoanhThu();
             //Hide paramater
             foreach (DevExpress.XtraReports.Parameters.Parameter p in report.Parameters)
                 p.Visible = false;
             //Init data report
-            report.InitData(fromday, today, data);
+            report.InitData(obj, data);
             documentViewer1.DocumentSource = report;
             report.CreateDocument();
         }
